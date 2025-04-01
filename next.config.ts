574c3ec0a1d6398c67df1a012c23baea70ca61b6
jdellamalva <59029670+jdellamalva@users.noreply.github.com>
2025-03-31 23:56:56 -0700

    updated config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: "export",
+    assetPrefix: "./",
+    basePath: "",
 };
 
 export default nextConfig;
