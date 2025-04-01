744d4382b0fe4885a3595b1e1172220f74280bf8
jdellamalva <59029670+jdellamalva@users.noreply.github.com>
2025-04-01 15:40:44 -0700

    test github actions

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,8 @@
 import type { NextConfig } from "next";
 
-const isProduction = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
-    basePath: isProduction ? "/jdellamalva.github.io" : "",
-    assetPrefix: isProduction ? "/jdellamalva.github.io" : "",
     output: "export",
+    trailingSlash: true,
 };
 
 export default nextConfig;
